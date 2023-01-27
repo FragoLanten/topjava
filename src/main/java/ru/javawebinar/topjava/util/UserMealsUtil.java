5f8afd4351fccfe8bd41fb770d9a25dcc70cc966
Fragolanten <rizinvadim@gmail.com>
2023-01-27 19:28:06 +0300

    реализовал метод filteredByCycles

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -22,39 +22,57 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
         );
 
-        List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(14, 0), 1500);
+        List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(14, 0), 2000);
         mealsTo.forEach(System.out::println);
 
 //        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 
+//    public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+//        List<UserMealWithExcess> list = new ArrayList<>();
+//        Set<Integer> daySet = new HashSet<>();
+//
+//        for (UserMeal meal:meals) {
+//            int day = meal.getDateTime().getDayOfMonth();
+//            daySet.add(day);
+//        }
+//        for (int certainDay:daySet) {
+//            int daysCalories=0;
+//            boolean caloriesExceed = false;
+//            for (UserMeal meal:meals) {
+//                LocalTime localTime = meal.getDateTime().toLocalTime();
+//                int dayOfMonth = meal.getDateTime().getDayOfMonth();
+//                if (dayOfMonth==certainDay && localTime.isAfter(startTime) && localTime.isBefore(endTime)) {
+//                    daysCalories+=meal.getCalories();
+//                }
+//                if (daysCalories>caloriesPerDay) {
+//                    caloriesExceed=true;
+//                }
+//            }
+//            for (UserMeal meal:meals) {
+//                LocalTime localTime = meal.getDateTime().toLocalTime();
+//                int dayOfMonth = meal.getDateTime().getDayOfMonth();
+//                if (dayOfMonth==certainDay && localTime.isAfter(startTime) && localTime.isBefore(endTime)) {
+//                    list.add(new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(),caloriesExceed));
+//                }
+//            }
+//        }
+//        return list;
+//    }
+
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         List<UserMealWithExcess> list = new ArrayList<>();
-        Set<Integer> daySet = new HashSet<>();
-
+        Map<Integer,Integer> dayMap = new HashMap<>();
         for (UserMeal meal:meals) {
-            int day = meal.getDateTime().getDayOfMonth();
-            daySet.add(day);
+            int currentDay = meal.getDateTime().getDayOfMonth();
+            dayMap.merge(currentDay, meal.getCalories(), Integer::sum);
         }
-        for (int certainDay:daySet) {
-            int daysCalories=0;
-            boolean caloriesExceed = false;
-            for (UserMeal meal:meals) {
-                LocalTime localTime = meal.getDateTime().toLocalTime();
-                int dayOfMonth = meal.getDateTime().getDayOfMonth();
-                if (dayOfMonth==certainDay && localTime.isAfter(startTime) && localTime.isBefore(endTime)) {
-                    daysCalories+=meal.getCalories();
-                }
-                if (daysCalories>caloriesPerDay) {
-                    caloriesExceed=true;
-                }
-            }
-            for (UserMeal meal:meals) {
-                LocalTime localTime = meal.getDateTime().toLocalTime();
-                int dayOfMonth = meal.getDateTime().getDayOfMonth();
-                if (dayOfMonth==certainDay && localTime.isAfter(startTime) && localTime.isBefore(endTime)) {
-                    list.add(new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(),caloriesExceed));
-                }
+        for (UserMeal meal:meals) {
+            LocalTime localTime = meal.getDateTime().toLocalTime();
+            int currentDay = meal.getDateTime().getDayOfMonth();
+            boolean exceed = caloriesPerDay<dayMap.get(currentDay);
+            if (localTime.isAfter(startTime) && localTime.isBefore(endTime)) {
+                list.add(new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(), exceed));
             }
         }
         return list;
