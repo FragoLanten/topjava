f4584f022ac081ed0207aa4bd963d94c774892a9
Fragolanten <rizinvadim@gmail.com>
2023-02-06 18:59:19 +0300

    отредактирована страница meals.jsp отрефакторил MealServlet в mealsUtil сделал лист продуктами доступным из всего приложения

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -25,17 +25,7 @@ public class MealServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to users");
 
-        List<Meal> meals = Arrays.asList(
-                new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
-                new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
-                new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
-                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
-                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
-                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
-                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
-        );
-
-        List<MealTo> mealsWithExceeded = MealsUtil.filteredByStreams(meals, LocalTime.of(0, 0), LocalTime.of(23, 0), 2000);
+        List<MealTo> mealsWithExceeded = MealsUtil.filteredByStreams(MealsUtil.meals, LocalTime.of(0, 0), LocalTime.of(23, 0), 2000);
         request.setAttribute("meals", mealsWithExceeded);
 
         request.getRequestDispatcher("meals.jsp").forward(request, response);
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,5 +1,6 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"  %>
 
+<%@ page import="java.time.format.DateTimeFormatter" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html lang="ru">
 <head>
@@ -10,8 +11,8 @@
 <hr>
 <h2>Meals</h2>
 
-<table>
-    <tr>
+<table border="1" rules="all" cellpadding="5">
+    <tr >
         <th>Date</th>
         <th>Description</th>
         <th>Calories</th>
@@ -19,10 +20,16 @@
 
 
     <c:forEach var="meal" items="${meals}">
-        <tr>
-            <td>${meal.dateTime}</td>
+        <c:if test="${meal.excess eq true}" >
+        <tr style="color:red">
+        </c:if>
+        <c:if test="${meal.excess eq false}" >
+            <tr style="color:green">
+        </c:if>
+            <td>${meal.dateTime.format(DateTimeFormatter.ofPattern("dd.MM.yyyy hh:mm"))}</td>
             <td>${meal.description}</td>
             <td>${meal.calories}</td>
+
         </tr>
     </c:forEach>
 
